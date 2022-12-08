c1c48dc2d143fd2bae141caec942bfd5dee46bce
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-12-07 23:15:57 -0300

    publish extras

---
+++ RabbitMQ/Demo2-publish/Extras.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2_publish
+{
+    public class Extras
+    {
+        public class ServiceXPTO
+        {
+            public void DoAnything(CustomObject message) { }
+        }
+
+        public class CustomObject
+        {
+            public string Text { get; set; }
+        }
+
+
+        public class Serializer
+        {
+            public T Deserialize<T>(byte[] messageBytes) => default(T);
+        }
+    }
+}
