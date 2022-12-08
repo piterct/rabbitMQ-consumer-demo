df81a52d992a1e9e5f1fbd5baa9f5e7416198a83
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-12-07 23:01:57 -0300

    extra class

---
+++ RabbitMQ/Demo1-consumer/Extras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_consumer
+{
+    public class ServiceXPTO
+    {
+        public void DoAnything(CustomObject message) { }
+    }
+
+    public class CustomObject
+    {
+        public string Text { get; set; }
+    }
+
+
+    public class Serializer
+    {
+        public T Deserialize<T>(byte[] messageBytes) => default(T);
+    }
+}
